7ad45a8904f8f5539e9f59e8430cee90b1156634
khawja Idrees <khwajaidrees22@gmail.com>
2025-07-15 12:07:07 -0700

    Updated blog summarizer: added features and fixed favicon

--- app/api/blog/route.ts
+++ app/api/blog/route.ts
@@ -1,7 +1,5 @@
 import { NextResponse } from "next/server";
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
+import { prisma } from "components/lib/prisma";
 
 export async function GET() {
     try {
